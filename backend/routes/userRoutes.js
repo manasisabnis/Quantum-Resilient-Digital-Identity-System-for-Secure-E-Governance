12b2fc8aed7a16310c7b2eb89e2f59ab87d1bd87
Rohini Sabnis <rohinisabnis@Rohinis-MacBook-Airlocal.local>
2025-10-30 20:03:10 +0530

    Removed incorrect backend folder

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser, verifyUser } = require("../controllers/userController");
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/verify/:email", verifyUser);
-
-module.exports = router;
